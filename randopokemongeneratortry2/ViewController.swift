196fe084f3644aeeb2b3c34425d12fe276b2eff8
Owen Lee <golem9005@gmail.com>
2019-07-18 09:53:09 -0700

    ayy I'm done

--- randopokemongeneratortry2/ViewController.swift
+++ randopokemongeneratortry2/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageview1: UIImageView!
+    var pictures = ["Pikachu", "Dratini", "Bulbasaur", "Charmander", "Meowth", "Snorlax", "Eevee"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func Button1(_ sender: Any) {
+        let RollNum = Int.random(in:0...6)
+        imageview1.image = UIImage (named: pictures[RollNum])
+    }
+    
+
 
 }
-
